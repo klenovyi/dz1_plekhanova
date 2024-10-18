6417a0017b674d8ddb9d86b6b835efc81e3212f2
klenovyi <siropchyk17@gmail.com>
2024-10-18 16:22:04 +0300

    change factorial.c in dev2

--- factorial.c
+++ factorial.c
@@ -2,11 +2,13 @@
 
 // Реализация функции для вычисления факториала
 unsigned long long factorial(int n) {
+    printf("DEV2 \n");
     if (n < 0) {
         return 0; // Факториал отрицательного числа не определен
     }
     if (n == 0 || n == 1) {
         return 1; // Факториал 0 и 1 равен 1
+
     }
     unsigned long long result = 1;
 
